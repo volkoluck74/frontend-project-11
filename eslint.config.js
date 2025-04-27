1487a47ffb958ac98a00638f8534f8f943dac5fd
Volkoluck <volko-ov@yandex.ru>
2025-04-27 13:35:10 +0000

    add new linter part 3

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,15 @@
 import globals from 'globals'
 
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import pluginJs from '@eslint/js'
+//import path from 'path'
+//import { fileURLToPath } from 'url'
+//import { FlatCompat } from '@eslint/eslintrc'
+//import pluginJs from '@eslint/js'
 import importPlugin from 'eslint-plugin-import'
 import stylistic from '@stylistic/eslint-plugin'
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+//const __filename = fileURLToPath(import.meta.url)
+//const __dirname = path.dirname(__filename)
 /*
 const compat = new FlatCompat({
   baseDirectory: __dirname,
